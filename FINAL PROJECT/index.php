432440d1f6e166877347ef4ac07fdb9e6337ff67
FranciscoCastro21 <135382533+FranciscoCastro21@users.noreply.github.com>
2023-12-10 13:32:16 -0500

    Update index.php

--- FINAL PROJECT/index.php
+++ FINAL PROJECT/index.php
@@ -4,7 +4,7 @@ session_start();
 
 if (isset($_SESSION["user_ID"])) {
     
-    $mysqli = require __DIR__ . "/dbh.php";
+    $mysqli = require __DIR__ . "dbh.php";
     
     $sql = "SELECT * FROM user
             WHERE id = {$_SESSION["user_ID"]}";
@@ -22,18 +22,18 @@ if (isset($_SESSION["user_ID"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="/css/home.css">
+    <link rel="stylesheet" href="home.css">
 </head>
 <body>
     <!-- Sorry couldn't solve the problem with the login, and the button to redirect to upload doesn't work, please use the nav bar to go anywhere :( I AM SORRYYYY -->
 
 <main>
-    <?php include ("../globalTemplates/header.php"); ?>
+    <?php include ("header.php"); ?>
 
     <section class="SignUp">
     <div class="container">
         <h2>Sign Up</h2>
-        <form action="/php/singUp.php" method="post">
+        <form action="singUp.php" method="post">
             <div class="form-group">
                 <label for="username">Username:</label>
                 <input type="text" class="form-control" id="username" name="username" required>
@@ -55,7 +55,7 @@ if (isset($_SESSION["user_ID"])) {
     <section class="Login">
         <div class="container">
             <h2>Login</h2>
-            <form action="./php/login.php" method="post">
+            <form action="login.php" method="post">
                 <div class="form-group">
                     <label for="email">Email:</label>
                     <input type="email" class="form-control" id="email" name="email" required>
@@ -65,12 +65,12 @@ if (isset($_SESSION["user_ID"])) {
                     <input type="password" class="form-control" id="user_password" name="user_password" required>
                 </div>
                 <div class="btn-container">
-                <a href="/navigtion/updates.php"><button type="submit" class="btn btn-primary">Login</button></a>
+                <a href="updates.php"><button type="submit" class="btn btn-primary">Login</button></a>
                 </div>
             </form>
         </div>
     </section>
-    <?php include ("../globalTemplates/footer.php"); ?>
+    <?php include ("footer.php"); ?>
 </main>
 </body>
 </html>
