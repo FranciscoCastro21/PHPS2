de032bce16a6a0047502165c9ab21113d8830e5e
FranciscoCastro21 <135382533+FranciscoCastro21@users.noreply.github.com>
2023-12-10 13:35:05 -0500

    Update edit.php

--- FINAL PROJECT/edit.php
+++ FINAL PROJECT/edit.php
@@ -1,5 +1,5 @@
 <?php
-include "C:\Users\User\Documents\PHP CRUD PROJECT\php\db2.php";
+include "C:\Users\User\Documents\PHP CRUD PROJECT\db2.php";
 
 
 $artID = isset($_GET["id"]) ? $_GET["id"] : die("Art ID not provided");
@@ -39,12 +39,12 @@ if (isset($_POST["submit"])) {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   <title>PHP EDIT</title>
-  <link rel="stylesheet" href="/css/crud.css">
+  <link rel="stylesheet" href="crud.css">
 </head>
 
 <body>
   
-  <?php include ("../globalTemplates/header.php"); ?>
+  <?php include ("header.php"); ?>
 
   <div class="container_edit">
     <div class="text-center mb-4">
@@ -96,7 +96,7 @@ if (isset($_POST["submit"])) {
       </form>
     </div>
   </div>
-  <?php include ("../globalTemplates/footer.php"); ?>
+  <?php include ("ooter.php"); ?>
 
   <!-- Bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
